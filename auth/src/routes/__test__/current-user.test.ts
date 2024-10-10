f3c44bf010e3cdfff748695506646d75b9f89b00
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-10 20:43:10 +0100

    working now

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -9,6 +9,6 @@ it("Responds with details about current user.", async () => {
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
     .send()
-    .expect(400);
+    .expect(200);
   expect(response.body.currentUser.email).toBe("test@test.com");
 });
