4c9ecba7227515ed2310eaae3dfbaae0affb6595
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-11-15 20:41:08 +0100

    final deploy test

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,7 +4,7 @@ import { rabbitMqWrapper } from "./rabbitmq-wrapper";
 import { TicketsServiceListener } from "./events/listeners/listener";
 
 const start = async () => {
-  console.log("Starting up...");
+  console.log("Starting up");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined.");
